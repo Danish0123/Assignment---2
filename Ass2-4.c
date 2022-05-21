9aa9b7b363a8344e42933d6d99826896ab1c6fb1
Danish <95996366+Danish0123@users.noreply.github.com>
2022-05-21 11:23:38 +0530

    Add files via upload

---
+++ Ass2-4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int num,rem,prod;
+	printf("Enter a Number:");
+	scanf("%d",&num);
+	for(prod=1;num>0;num=num/10)
+	{
+		rem=num%10;
+		prod=prod*rem;
+	}
+	printf("product of digits of this number = %d",prod);
+	return 0;
+}
